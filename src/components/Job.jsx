daa8f80b0b7cb4801a1ecc2f54f5b716d4991935
Caffeinated Misfit <rpattersonit@gmail.com>
2024-02-27 19:53:46 -0500

    Add files via upload

---
+++ src/components/Job.jsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom"
+
+const Job = () => {
+    return (
+        <>
+        <h1>Good job Rhonda</h1>
+        <Link to ="/">Link to Home</Link>
+        </>
+
+    )
+}
+export default Job
